7bff8dc23717edc8c97ec835aa120f92257dc8ee
Luciano Moffatt <lmoffatt@gmail.com>
2024-06-06 14:47:00 -0300

    Initial commit

---
+++ R/ConformationalModel.R
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+ConformationalModel<-function(Agonist_dependencies,
+                              conformational_change_scheme,
+                              conformational_chage_interactions,
+                              conductance_interactions,
+                              standard_states,
+                              conductance_interaction_info)
+{
+
+
+}
+
